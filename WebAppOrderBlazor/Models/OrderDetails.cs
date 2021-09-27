3bceca9d683449ab805278b6a69e403a60c7d8f3
Henry Jara <52138733+HenryDaniJP88@users.noreply.github.com>
2021-09-27 16:07:26 -0400

    Agregar archivos de proyecto.

---
+++ WebAppOrderBlazor/Models/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppOrderBlazor.Models
+{
+    public class OrderDetails
+    {
+        public int OrderDetailNo { get; set; }
+        public int OrderNo { get; set; }
+        public string ItemName { get; set; }
+        public string Notes { get; set; }
+        public int Qty { get; set; }
+        public int Price { get; set; }
+    }
+}
